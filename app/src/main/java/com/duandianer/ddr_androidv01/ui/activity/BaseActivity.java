3f6a3e79c0c942bf3cfbf98e36e4861a2accb018
Du Rong <d.rong@outlook.com>
2016-12-08 22:24:52 +0800

    DDR_Android

---
+++ app/src/main/java/com/duandianer/ddr_androidv01/ui/activity/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.duandianer.ddr_androidv01.ui.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * Created by Administrator on 12/8/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/duandianer/ddr_androidv01/ui/activity/HomePageActivity.java
@@ -0,0 +1,74 @@
+/**
+ * CopyRright (c)2016:   <短点>
+ * Project:              <短点Android>
+ * Module ID:            <>
+ * Comments:  <主页Activity>
+ * JDK version used:      <JDK1.6>
+ * Author：        <@奔跑的MT>
+ * Create Date：  <2016-12-03>
+ * Modified By：   <@奔跑的MT>
+ * Modified Date:  <2016-12-03>
+ * Why & What is modified  <修改原因描述>
+ * Version:                  <V0.1>
+ */
+package com.duandianer.ddr_androidv01.ui.activity;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
+
+import com.duandianer.ddr_androidv01.R;
+import com.duandianer.ddr_androidv01.ui.fragment.Find_fragment;
+import com.duandianer.ddr_androidv01.ui.fragment.My_fragment;
+import com.duandianer.ddr_androidv01.ui.fragment.Selected_fragment;
+
+public class HomePageActivity extends BaseActivity {
+
+    private FragmentTabHost mFragmentTabHost;
+    private TabWidget mTabWidget;
+
+    private TabHost.TabSpec mSelected;
+    private TabHost.TabSpec mFind;
+    private TabHost.TabSpec mMy;
+
+    private Toolbar mToolbar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_home_page);
+
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle("");
+        setSupportActionBar(mToolbar);
+
+
+        mFragmentTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mFragmentTabHost.setup(getApplicationContext(), getSupportFragmentManager(), android.R.id.tabcontent);
+
+        mSelected = mFragmentTabHost.newTabSpec("selected").setIndicator(getView(R.layout.icon_selected));
+        mFind = mFragmentTabHost.newTabSpec("find").setIndicator(getView(R.layout.icon_find));
+        mMy = mFragmentTabHost.newTabSpec("my").setIndicator(getView(R.layout.icon_my));
+
+        mFragmentTabHost.addTab(mSelected, Selected_fragment.class, null);
+        mFragmentTabHost.addTab(mFind, Find_fragment.class, null);
+        mFragmentTabHost.addTab(mMy, My_fragment.class, null);
+
+    }
+
+    private View getView(int id) {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(id, null);
+        return view;
+    }
+}
